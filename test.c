9808869b504b4649a293520fb8d55110cb9289d8
iamcaye <cayetano.biehler@gmail.com>
2020-10-09 11:51:20 +0200

    first class

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+void *SayHi (void *threadid) {
+  long tid = (*(long *)threadid);
+  printf("Hola estamos aprendiendo a usar la libreria pthread de POSIX\n");
+  printf("Soy %ld\n", tid);
+}
+
+int main (int argc, void * argv[]) {
+  pthread_t thread[5];
+  long t[5];
+  int rc;
+
+  for (unsigned i = 0 ; i < 5 ; i++) {
+    t[i] = i;
+    rc = pthread_create(&thread[i], NULL, SayHi, &t[i]);
+    if(rc < 0){
+      printf("Fallo en pthread_create %d", i);
+      exit(0);
+    }
+  }
+  pthread_exit(NULL);
+  return 0;
+}
