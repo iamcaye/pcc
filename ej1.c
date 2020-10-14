1a1bc68d7fbad568eeefc44bd5b4d5a5536cd5cb
iamcaye <cayetano.biehler@gmail.com>
2020-10-14 09:44:45 +0200

    pthread_join done

--- ej1.c
+++ ej1.c
@@ -15,11 +15,13 @@ void *printChar (void * argum){
   for (unsigned j = 0 ; j < args -> n ; j++) {
     printf("%s\t", args -> c);
   }
+  printf("\n");
 }
 
 int main (int argc, void * argv[]) {
   pthread_t threads[NTHREADS];
   struct arg_struct args[NTHREADS];
+  void * status;
   int rc;
 
   for (unsigned i = 0 ; i < NTHREADS ; i++) {
@@ -28,6 +30,13 @@ int main (int argc, void * argv[]) {
     rc = pthread_create(&threads[i], NULL, printChar, (void *)&args[i]);
   }
 
-  pthread_exit(NULL);
+  for (unsigned h = 0 ; h < NTHREADS ; h++) {
+    rc = pthread_join(threads[h], &status);
+    if (rc != 0) {
+      printf("ERROR pthread_join() is %d\n", rc);
+      exit(-1);
+    }
+    printf("Fin thread %d estado: %ld\n", h, (long)status);
+  }
   return 0;
 }
