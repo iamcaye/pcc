85c73a5c8c1a56b77400f4e030e5541f255d32db
iamcaye <cayetano.biehler@gmail.es>
2020-11-08 13:46:26 +0100

    ej2 and 3 done

---
+++ t3/ej2.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <pthread.h>
+
+int arr[100][2] = {[0 ... 99] = {-1, -1}};
+sem_t s_c[3];
+
+void * colocador(void * arg){
+  int c = *(int *)arg, cont = 0;
+  while(1){
+    for(unsigned j = 0 ; j < 3 ; j++) {
+      while(arr[cont][1] != -1){
+        cont++;
+        if(cont == 100){
+          cont = 0;
+        }
+      }
+      arr[cont][0] = rand() % 11;
+      arr[cont][1] = j;
+      printf("ID Producto %d = %d\n", arr[cont][1], arr[cont][0]);
+      sem_post(&s_c[j]);
+    }
+  printf("Hola\n");
+  }
+}
+
+void * robot (void * arg){
+  int t = *(int *)arg, n = 0;
+  while(1){
+    printf("%d intenta recoger\n", t);
+    sem_wait(&s_c[t]);
+    while(arr[n][1] != t){
+      n++;
+      if(n >= 100){
+        n = 0;
+      }
+    }
+    printf("Robot %d retira %d\n", t, arr[n][0]);
+    arr[n][1] = -1;
+  }
+}
+
+int main (int argc, char *argv[]){
+  pthread_t threads[4];
+  int rc, c = 100, t[3];
+  void *status;
+
+  for(unsigned k =  0 ; k < 3 ; k++){
+    sem_init(&s_c[k], 0, 0);
+  }
+
+  if(argc > 1) {
+    c = atoi(argv[1]);
+  }
+  
+
+
+  for(unsigned i = 1 ; i < 4 ; i++){
+    t[i] = i-1;
+    printf("creado %d", t[i]);
+    rc = pthread_create(&threads[i], NULL, robot, (void *)&t[i]);
+    if(rc != 0){
+      perror("Fallo en pthread_create colocador");
+      exit(-1);
+    }
+  }
+
+  rc = pthread_create(&threads[0], NULL, colocador, (void *)&c);
+  if(rc != 0){
+    perror("Fallo en pthread_create colocador");
+    exit(-1);
+  }
+
+  for (long h = 0 ; h < 4 ; h++) {
+    rc = pthread_join(threads[h], &status);
+    if (rc != 0) {
+      printf("ERROR pthread_join() is %d\n", rc);
+      exit(-1);
+    }
+    printf("Fin thread %ld estado: %ld\n", h, (long)status);
+  }
+}
---
+++ t3/ej3.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define TAM 1000
+int arr[TAM] = {[0 ... TAM-1] = -1};
+int turno = 0;
+int f[2] = {0,0};
+sem_t s_sur, s_norte;
+
+void * sur (void * arg){
+  int N = *(int *)arg, k = 0;
+  while(1){
+    sem_wait(&s_sur);
+    f[0] = 1;
+    turno = 1;
+    printf("SUR comprobando pista\n");
+    while(turno == 1 && f[1])
+      ;
+    while(arr[k] != 1){
+      k++;
+      if(k == N){
+        k = 0;
+      }
+    }
+    arr[k] = -1;
+    printf("Avion %d ha aterrizado desde el SUR\n", k);
+    f[0] = 0;
+  }
+}
+
+void * norte (void * arg){
+  int N = *(int *)arg, k = 0;
+  while(1){
+    sem_wait(&s_norte);
+    f[1] = 1;
+    turno = 0;
+    printf("NORTE comprobando pista\n");
+    while(turno == 0 && f[0])
+      ;
+    while(arr[k] != 0){
+      k++;
+      if(k == N){
+        k = 0;
+      }
+    }
+    arr[k] = -1;
+    printf("Avion %d ha aterrizado desde el NORTE\n", k);
+    f[1] = 0;
+  }
+}
+
+void * avion(void * arg){
+  int av = *(int *)arg;
+  while(1){
+    if(arr[av] == -1){
+      arr[av] = rand() % 2;
+      if(arr[av] == 0){
+        printf("Avion %d solicita permiso para aterrrizar desde el NORTE\n", av);
+        sem_post(&s_norte);
+      }else{
+        printf("Avion %d solicita permiso para aterrrizar desde el SUR\n", av);
+        sem_post(&s_sur);
+      }
+    }
+  }
+}
+
+int main(int argc, char * argv[]) {
+  int N = 10, rc, t[N];
+  if(argc > 1) {
+    N = atoi(argv[1]);
+  }
+  pthread_t threads[N+2];
+  sem_init(&s_sur, 0,0);
+  sem_init(&s_norte, 0,0);
+  void *status;
+
+  rc = pthread_create(&threads[0], NULL, norte, (void *)&N);
+  if(rc != 0){
+    perror("Fallo en pthread_create norte");
+    exit(-1);
+  }
+
+  rc = pthread_create(&threads[1], NULL, sur, (void *)&N);
+  if(rc != 0){
+    perror("Fallo en pthread_create sur");
+    exit(-1);
+  }
+
+  for(unsigned i = 0 ; i < N ; i++){
+    t[i] = i;
+    printf("Avion %d despega\n", i);
+    rc = pthread_create(&threads[i+2], NULL, avion, (void *)&t[i]);
+    if(rc != 0){
+      perror("Fallo en pthread_create avion");
+      exit(-1);
+    }
+  }
+
+  for (long h = 0 ; h < N+2 ; h++) {
+    rc = pthread_join(threads[h], &status);
+    if (rc != 0) {
+      printf("ERROR pthread_join() is %d\n", rc);
+      exit(-1);
+    }
+    printf("Fin thread %ld estado: %ld\n", h, (long)status);
+  }
+  return 0;
+}
